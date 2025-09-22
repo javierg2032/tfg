4d5ca9804cfa764b73049e93565973bf01c40090
Javier Galán Cortés <javier.galan2032@gmail.com>
2025-09-22 17:33:04 +0200

    Nuevas Tablas

--- database/ryujin.sql
+++ database/ryujin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-09-2025 a las 10:36:42
+-- Tiempo de generación: 22-09-2025 a las 17:31:58
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ryujin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `anuncios`
+--
+
+CREATE TABLE `anuncios` (
+  `id_anuncio` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `imagen` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `anuncios`
+--
+
+INSERT INTO `anuncios` (`id_anuncio`, `nombre`, `imagen`) VALUES
+(1, 'Anuncio Pokémon', '/assets/Anuncio-Pokémon.png'),
+(2, 'Anuncio Digimon', '/assets/Anuncio-Digimon.png'),
+(3, 'Anuncio Magic', '/assets/Anuncio-Magic.png');
 
 -- --------------------------------------------------------
 
@@ -46,6 +67,23 @@ INSERT INTO `categorias` (`id_categoria`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `direcciones`
+--
+
+CREATE TABLE `direcciones` (
+  `id_direccion` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `calle` varchar(255) NOT NULL,
+  `ciudad` varchar(100) NOT NULL,
+  `codigo_postal` varchar(20) NOT NULL,
+  `provincia` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `productos`
 --
 
@@ -55,26 +93,87 @@ CREATE TABLE `productos` (
   `id_categoria` int(11) NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   `stock` int(11) DEFAULT 0,
-  `imagen` varchar(255) DEFAULT NULL
+  `imagen` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_producto`, `nombre`, `id_categoria`, `precio`, `stock`, `imagen`) VALUES
-(1, 'Pokemon TCG Megaevoluciones Booster Box ESP', 1, 189.99, 20, '/assets/PokemonTCGMegaevolucionesBoosterBoxESP.jpg\r\n'),
-(2, 'Pokemon TCG Megaevoluciones Elite Trainer Box ESP M-Garedevoir', 1, 59.99, 15, '/assets/PokemonTCGMegaevolucionesEliteTrainerBoxESPM-Garedevoir.jpg'),
-(3, 'Pokemon TCG Megaevoluciones Elite Trainer Box ESP M-Lucario', 1, 59.99, 15, '/assets/PokemonTCGMegaevolucionesEliteTrainerBoxESPM-Lucario.jpg'),
-(4, 'Magic The Gathering ATLA Booster Box ENG', 3, 159.99, 5, '/assets/MagicTheGatheringATLABoosterBoxENG.jpg'),
-(5, 'Magic The Gathering ATLA Collector Boosters Box ENG', 3, 459.99, 3, '/assets/MagicTheGatheringATLACollectorBoostersBoxENG.jpg'),
-(6, 'Magic The Gathering ATLA Bundle ENG', 3, 69.99, 25, '/assets/MagicTheGatheringATLABundleENG.jpg'),
-(7, 'Magic The Gathering ATLA Comander\'s Bundle ENG', 3, 54.99, 20, '/assets/MagicTheGatheringATLAComander\'sBundleENG.jpg'),
-(8, 'MagicTheGatheringATLAJumpstartBoostersENG', 3, 179.99, 7, '/assets/MagicTheGatheringATLAJumpstartBoostersENG.jpg');
+INSERT INTO `productos` (`id_producto`, `nombre`, `id_categoria`, `precio`, `stock`, `imagen`, `descripcion`) VALUES
+(1, 'Pokemon TCG Megaevoluciones Booster Box ESP', 1, 189.99, 20, '/assets/PokemonTCGMegaevolucionesBoosterBoxESP.jpg\r\n', NULL),
+(2, 'Pokemon TCG Megaevoluciones Elite Trainer Box ESP M-Garedevoir', 1, 59.99, 15, '/assets/PokemonTCGMegaevolucionesEliteTrainerBoxESPM-Garedevoir.jpg', NULL),
+(3, 'Pokemon TCG Megaevoluciones Elite Trainer Box ESP M-Lucario', 1, 59.99, 15, '/assets/PokemonTCGMegaevolucionesEliteTrainerBoxESPM-Lucario.jpg', NULL),
+(4, 'Magic The Gathering ATLA Booster Box ENG', 3, 159.99, 5, '/assets/MagicTheGatheringATLABoosterBoxENG.jpg', 'Lucha codo con codo con maestros de los elementos, animales híbridos y aliados entrañables que empuñan espadas, búmeran y algún que otro repollo.'),
+(5, 'Magic The Gathering ATLA Collector Boosters Box ENG', 3, 459.99, 3, '/assets/MagicTheGatheringATLACollectorBoostersBoxENG.jpg', '¡Explora el mundo de Avatar: la leyenda de Aang! Hazte con las cartas más buscadas de los aliados inolvidables, los animales híbridos y los enemigos terribles de las cuatro naciones.'),
+(6, 'Magic The Gathering ATLA Bundle ENG', 3, 69.99, 25, '/assets/MagicTheGatheringATLABundleENG.jpg', 'Dominar los elementos es el primer paso de tu viaje en Magic: The Gathering. Practica, controla tu arquetipo de maná y busca el equilibrio de tu mazo.'),
+(7, 'Magic The Gathering ATLA Comander\'s Bundle ENG', 3, 54.99, 20, '/assets/MagicTheGatheringATLAComander\'sBundleENG.jpg', 'Este bundle maestro une los elementos clave de la colección y contiene cartas esenciales de Commander para construir el mazo que este mundo necesita.'),
+(8, 'MagicTheGatheringATLAJumpstartBoostersENG', 3, 179.99, 7, '/assets/MagicTheGatheringATLAJumpstartBoostersENG.jpg', 'Los animales híbridos son el doble de divertidos, y con Jumpstart juntas dos sobres para tener listo un mazo. ¡Ya puedes jugar, así que yip, yip!'),
+(9, 'Magic The Gathering ATLA Booster Box ESP', 3, 159.99, 4, '/assets/MagicTheGatheringATLABoosterBoxESP.jpg', 'Lucha codo con codo con maestros de los elementos, animales híbridos y aliados entrañables que empuñan espadas, búmeran y algún que otro repollo.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_direccion`
+--
+
+CREATE TABLE `tipos_direccion` (
+  `id_tipo` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipos_direccion`
+--
+
+INSERT INTO `tipos_direccion` (`id_tipo`, `nombre`) VALUES
+(1, 'envío'),
+(2, 'facturación');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `correo`, `contrasena`, `fecha_registro`) VALUES
+(1, 'Ñulcrum', 'javier.galan2032@gmail.com', '$2y$10$H.Qx4M8u96X5H6nvsz72cOFUo.0l1jelGvc7TZ5.h.muL4mjTRvSy', '2025-09-22 08:33:56');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_direcciones`
+--
+
+CREATE TABLE `usuarios_direcciones` (
+  `id_usuario_direccion` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_direccion` int(11) NOT NULL,
+  `id_tipo` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `anuncios`
+--
+ALTER TABLE `anuncios`
+  ADD PRIMARY KEY (`id_anuncio`);
 
 --
 -- Indices de la tabla `categorias`
@@ -84,6 +183,12 @@ ALTER TABLE `categorias`
   ADD UNIQUE KEY `nombre` (`nombre`);
 
 --
+-- Indices de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  ADD PRIMARY KEY (`id_direccion`);
+
+--
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
@@ -91,8 +196,38 @@ ALTER TABLE `productos`
   ADD KEY `id_categoria` (`id_categoria`);
 
 --
+-- Indices de la tabla `tipos_direccion`
+--
+ALTER TABLE `tipos_direccion`
+  ADD PRIMARY KEY (`id_tipo`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- Indices de la tabla `usuarios_direcciones`
+--
+ALTER TABLE `usuarios_direcciones`
+  ADD PRIMARY KEY (`id_usuario_direccion`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_direccion` (`id_direccion`),
+  ADD KEY `id_tipo` (`id_tipo`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `anuncios`
+--
+ALTER TABLE `anuncios`
+  MODIFY `id_anuncio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
@@ -101,10 +236,34 @@ ALTER TABLE `categorias`
   MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  MODIFY `id_direccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `tipos_direccion`
+--
+ALTER TABLE `tipos_direccion`
+  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_direcciones`
+--
+ALTER TABLE `usuarios_direcciones`
+  MODIFY `id_usuario_direccion` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -115,6 +274,14 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `productos`
   ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios_direcciones`
+--
+ALTER TABLE `usuarios_direcciones`
+  ADD CONSTRAINT `usuarios_direcciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `usuarios_direcciones_ibfk_2` FOREIGN KEY (`id_direccion`) REFERENCES `direcciones` (`id_direccion`) ON DELETE CASCADE,
+  ADD CONSTRAINT `usuarios_direcciones_ibfk_3` FOREIGN KEY (`id_tipo`) REFERENCES `tipos_direccion` (`id_tipo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
